d6e9a364ef0b3e9654b5192f5f28e19eb5ef80c3
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-10 22:33:57 +0700

    Add files via upload

---
+++ Latihan3/FormBelahKetupat/FormBelahKetupat/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim diagonal1, diagonal2, sisi, luas, keliling As Integer
+
+        diagonal1 = Val(txtDiagonal1.Text)
+        diagonal2 = Val(txtDiagonal2.Text)
+        sisi = Val(txtSisi.Text)
+
+        luas = 0.5 * diagonal1 * diagonal2
+        keliling = 4 * sisi
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+End Class
