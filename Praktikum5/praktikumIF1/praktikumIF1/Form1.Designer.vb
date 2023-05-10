8437a1d5cd1d0d817402c46d45d7615e62a02642
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-10 23:43:29 +0700

    Add files via upload

---
+++ Praktikum5/praktikumIF1/praktikumIF1/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtGajiBulanan = New System.Windows.Forms.TextBox()
+        Me.txtKeterangan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Gaji Bulanan"
+        '
+        'txtGajiBulanan
+        '
+        Me.txtGajiBulanan.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.txtGajiBulanan.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.txtGajiBulanan.Location = New System.Drawing.Point(43, 54)
+        Me.txtGajiBulanan.Name = "txtGajiBulanan"
+        Me.txtGajiBulanan.Size = New System.Drawing.Size(159, 20)
+        Me.txtGajiBulanan.TabIndex = 1
+        '
+        'txtKeterangan
+        '
+        Me.txtKeterangan.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.txtKeterangan.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.txtKeterangan.Location = New System.Drawing.Point(43, 123)
+        Me.txtKeterangan.Name = "txtKeterangan"
+        Me.txtKeterangan.Size = New System.Drawing.Size(335, 20)
+        Me.txtKeterangan.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Keterangan"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnSubmit.Location = New System.Drawing.Point(303, 54)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 4
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(239, 161)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Robby A"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(420, 186)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtKeterangan)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtGajiBulanan)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Bantuan Dana Desa Cikalahang"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtGajiBulanan As TextBox
+    Friend WithEvents txtKeterangan As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Praktikum5/praktikumIF1/praktikumIF1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If (Val(txtGajiBulanan.Text) >= 5000000) Then
+            txtKeterangan.Text = "Mohon maaf anda tidak bisa mendapatkan bantuan dari Desa"
+        Else
+            txtKeterangan.Text = "Selamat anda mendapatkan bantuan dari Desa"
+        End If
+    End Sub
+End Class
