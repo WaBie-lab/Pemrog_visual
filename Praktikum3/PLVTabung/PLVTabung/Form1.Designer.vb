5e7b902ab9f30b58f78034b6c893dc9cb53551a5
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-10 22:54:01 +0700

    Add files via upload

---
+++ Praktikum3/PLVTabung/PLVTabung/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtJarijari = New System.Windows.Forms.TextBox()
+        Me.txtTinggi = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.txtVolume = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtLuas = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(307, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Menghitung Luas Permukaan &  Volume Tabung"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jarijari"
+        '
+        'txtJarijari
+        '
+        Me.txtJarijari.Location = New System.Drawing.Point(145, 42)
+        Me.txtJarijari.Name = "txtJarijari"
+        Me.txtJarijari.Size = New System.Drawing.Size(100, 20)
+        Me.txtJarijari.TabIndex = 2
+        '
+        'txtTinggi
+        '
+        Me.txtTinggi.Location = New System.Drawing.Point(145, 68)
+        Me.txtTinggi.Name = "txtTinggi"
+        Me.txtTinggi.Size = New System.Drawing.Size(100, 20)
+        Me.txtTinggi.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Tinggi"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.btnHitung.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnHitung.Location = New System.Drawing.Point(145, 105)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(75, 23)
+        Me.btnHitung.TabIndex = 5
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = False
+        '
+        'txtVolume
+        '
+        Me.txtVolume.Location = New System.Drawing.Point(145, 178)
+        Me.txtVolume.Name = "txtVolume"
+        Me.txtVolume.Size = New System.Drawing.Size(100, 20)
+        Me.txtVolume.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(55, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(42, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Volume"
+        '
+        'txtLuas
+        '
+        Me.txtLuas.Location = New System.Drawing.Point(145, 152)
+        Me.txtLuas.Name = "txtLuas"
+        Me.txtLuas.Size = New System.Drawing.Size(100, 20)
+        Me.txtLuas.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(55, 152)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Luas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ClientSize = New System.Drawing.Size(337, 245)
+        Me.Controls.Add(Me.txtVolume)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtLuas)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtTinggi)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtJarijari)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtJarijari As TextBox
+    Friend WithEvents txtTinggi As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtVolume As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Praktikum3/PLVTabung/PLVTabung/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim jarijari, tinggi, luasp, volume As Integer
+
+        jarijari = Val(txtJarijari.Text)
+        tinggi = Val(txtTinggi.Text)
+
+        luasp = 2 * 3.14 * jarijari * (jarijari + tinggi)
+        volume = 3.14 * jarijari * jarijari * tinggi
+
+        txtLuas.Text = Str(luasp)
+        txtVolume.Text = Str(volume)
+    End Sub
+End Class
