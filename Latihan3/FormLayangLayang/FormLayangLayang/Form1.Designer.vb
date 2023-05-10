ef0bf77614a140b1005310e9d8019f3d5990ad13
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-10 22:36:14 +0700

    Add files via upload

---
+++ Latihan3/FormLayangLayang/FormLayangLayang/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtDiagonal1 = New System.Windows.Forms.TextBox()
+        Me.txtDiagonal2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSisi1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSisi2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSisi3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtSisi4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.txtKeliling = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtLuas = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Diagonal1"
+        '
+        'txtDiagonal1
+        '
+        Me.txtDiagonal1.Location = New System.Drawing.Point(106, 38)
+        Me.txtDiagonal1.Name = "txtDiagonal1"
+        Me.txtDiagonal1.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiagonal1.TabIndex = 1
+        '
+        'txtDiagonal2
+        '
+        Me.txtDiagonal2.Location = New System.Drawing.Point(106, 64)
+        Me.txtDiagonal2.Name = "txtDiagonal2"
+        Me.txtDiagonal2.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiagonal2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Diagonal2"
+        '
+        'txtSisi1
+        '
+        Me.txtSisi1.Location = New System.Drawing.Point(106, 90)
+        Me.txtSisi1.Name = "txtSisi1"
+        Me.txtSisi1.Size = New System.Drawing.Size(100, 20)
+        Me.txtSisi1.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Sisi1"
+        '
+        'txtSisi2
+        '
+        Me.txtSisi2.Location = New System.Drawing.Point(106, 116)
+        Me.txtSisi2.Name = "txtSisi2"
+        Me.txtSisi2.Size = New System.Drawing.Size(100, 20)
+        Me.txtSisi2.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Sisi2"
+        '
+        'txtSisi3
+        '
+        Me.txtSisi3.Location = New System.Drawing.Point(106, 142)
+        Me.txtSisi3.Name = "txtSisi3"
+        Me.txtSisi3.Size = New System.Drawing.Size(100, 20)
+        Me.txtSisi3.TabIndex = 9
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(27, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Sisi3"
+        '
+        'txtSisi4
+        '
+        Me.txtSisi4.Location = New System.Drawing.Point(106, 168)
+        Me.txtSisi4.Name = "txtSisi4"
+        Me.txtSisi4.Size = New System.Drawing.Size(100, 20)
+        Me.txtSisi4.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(27, 168)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Sisi4"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.Location = New System.Drawing.Point(106, 195)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(75, 23)
+        Me.btnHitung.TabIndex = 12
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = True
+        '
+        'txtKeliling
+        '
+        Me.txtKeliling.Location = New System.Drawing.Point(106, 254)
+        Me.txtKeliling.Name = "txtKeliling"
+        Me.txtKeliling.Size = New System.Drawing.Size(100, 20)
+        Me.txtKeliling.TabIndex = 16
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(27, 254)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Keliling"
+        '
+        'txtLuas
+        '
+        Me.txtLuas.Location = New System.Drawing.Point(106, 228)
+        Me.txtLuas.Name = "txtLuas"
+        Me.txtLuas.Size = New System.Drawing.Size(100, 20)
+        Me.txtLuas.TabIndex = 14
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(27, 228)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(30, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "Luas"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 308)
+        Me.Controls.Add(Me.txtKeliling)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtLuas)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtSisi4)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtSisi3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSisi2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtSisi1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtDiagonal2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtDiagonal1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtDiagonal1 As TextBox
+    Friend WithEvents txtDiagonal2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSisi1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtSisi2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSisi3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtSisi4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtKeliling As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Latihan3/FormLayangLayang/FormLayangLayang/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim diagonal1, diagonal2, sisi1, sisi2, sisi3, sisi4, luas, keliling As Integer
+
+        diagonal1 = Val(txtDiagonal1.Text)
+        diagonal2 = Val(txtDiagonal2.Text)
+        sisi1 = Val(txtSisi1.Text)
+        sisi2 = Val(txtSisi2.Text)
+        sisi3 = Val(txtSisi3.Text)
+        sisi4 = Val(txtSisi4.Text)
+
+        luas = (diagonal1 * diagonal2) / 2
+        keliling = sisi1 + sisi2 + sisi3 + sisi4
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+End Class
