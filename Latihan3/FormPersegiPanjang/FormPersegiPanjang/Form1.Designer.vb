4ce04975c11c4d841d1ed13daabc038a7ab70695
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-10 22:43:47 +0700

    Add files via upload

---
+++ Latihan3/FormPersegiPanjang/FormPersegiPanjang/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPanjang = New System.Windows.Forms.TextBox()
+        Me.txtLebar = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.txtLuas = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtKeliling = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Panjang"
+        '
+        'txtPanjang
+        '
+        Me.txtPanjang.Location = New System.Drawing.Point(109, 46)
+        Me.txtPanjang.Name = "txtPanjang"
+        Me.txtPanjang.Size = New System.Drawing.Size(100, 20)
+        Me.txtPanjang.TabIndex = 1
+        '
+        'txtLebar
+        '
+        Me.txtLebar.Location = New System.Drawing.Point(109, 72)
+        Me.txtLebar.Name = "txtLebar"
+        Me.txtLebar.Size = New System.Drawing.Size(100, 20)
+        Me.txtLebar.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Lebar"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnHitung.Location = New System.Drawing.Point(109, 99)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(75, 23)
+        Me.btnHitung.TabIndex = 4
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = False
+        '
+        'txtLuas
+        '
+        Me.txtLuas.Location = New System.Drawing.Point(109, 144)
+        Me.txtLuas.Name = "txtLuas"
+        Me.txtLuas.Size = New System.Drawing.Size(100, 20)
+        Me.txtLuas.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Luas"
+        '
+        'txtKeliling
+        '
+        Me.txtKeliling.Location = New System.Drawing.Point(109, 170)
+        Me.txtKeliling.Name = "txtKeliling"
+        Me.txtKeliling.Size = New System.Drawing.Size(100, 20)
+        Me.txtKeliling.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Keliling"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.txtKeliling)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtLuas)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtLebar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPanjang)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPanjang As TextBox
+    Friend WithEvents txtLebar As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtKeliling As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Latihan3/FormPersegiPanjang/FormPersegiPanjang/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim panjang, lebar, luas, keliling As Integer
+
+        panjang = Val(txtPanjang.Text)
+        lebar = Val(txtLebar.Text)
+
+        luas = panjang * lebar
+        keliling = 2 * (panjang + lebar)
+
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+End Class
