633c57c152277f2fe05971a8d5e0f9184225aadb
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-11 08:54:07 +0700

    Add files via upload

---
+++ Praktikum4/praktikum4/Form_Farenheit/Form_Farenheit/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtFahrenheit = New System.Windows.Forms.TextBox()
+        Me.txtCelcius = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtReamur = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtKelvin = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(39, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Celcius"
+        '
+        'txtFahrenheit
+        '
+        Me.txtFahrenheit.Location = New System.Drawing.Point(117, 42)
+        Me.txtFahrenheit.Name = "txtFahrenheit"
+        Me.txtFahrenheit.Size = New System.Drawing.Size(100, 20)
+        Me.txtFahrenheit.TabIndex = 1
+        '
+        'txtCelcius
+        '
+        Me.txtCelcius.Location = New System.Drawing.Point(117, 115)
+        Me.txtCelcius.Name = "txtCelcius"
+        Me.txtCelcius.Size = New System.Drawing.Size(100, 20)
+        Me.txtCelcius.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label2.Location = New System.Drawing.Point(39, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Fahrenheit"
+        '
+        'txtReamur
+        '
+        Me.txtReamur.Location = New System.Drawing.Point(117, 144)
+        Me.txtReamur.Name = "txtReamur"
+        Me.txtReamur.Size = New System.Drawing.Size(100, 20)
+        Me.txtReamur.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label3.Location = New System.Drawing.Point(39, 144)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Reamur"
+        '
+        'txtKelvin
+        '
+        Me.txtKelvin.Location = New System.Drawing.Point(117, 170)
+        Me.txtKelvin.Name = "txtKelvin"
+        Me.txtKelvin.Size = New System.Drawing.Size(100, 20)
+        Me.txtKelvin.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label4.Location = New System.Drawing.Point(39, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Kelvin"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnHitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHitung.Location = New System.Drawing.Point(130, 72)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(75, 23)
+        Me.btnHitung.TabIndex = 8
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.txtKelvin)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtReamur)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtCelcius)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtFahrenheit)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtFahrenheit As TextBox
+    Friend WithEvents txtCelcius As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtReamur As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtKelvin As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnHitung As Button
+End Class
---
+++ Praktikum4/praktikum4/Form_Farenheit/Form_Farenheit/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim F, C, R, K As Integer
+        F = CDec(txtFahrenheit.Text)
+        C = ToCelcius(F)
+        R = ToReamur(F)
+        K = ToKelvin(F)
+
+        txtCelcius.Text = Str(C)
+        txtReamur.Text = Str(R)
+        txtKelvin.Text = Str(K)
+    End Sub
+
+    Private Function ToCelcius(F As Integer) As Decimal
+        Dim C As Integer
+        C = 5 / 9 * (F - 32)
+        Return C
+    End Function
+
+    Private Function ToReamur(F As Integer) As Decimal
+        Dim R As Integer
+        R = 4 / 9 * (F - 32)
+        Return R
+    End Function
+
+    Private Function ToKelvin(F As Integer) As Decimal
+        Dim K As Integer
+        K = 5 / 9 * (F - 32) + 273
+        Return K
+    End Function
+
+End Class
+
