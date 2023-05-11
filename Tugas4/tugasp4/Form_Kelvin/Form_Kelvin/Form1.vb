1954cf5fc81f982a3fdc9567f1ed946213ef28d5
WaBie-lab <70514983+WaBie-lab@users.noreply.github.com>
2023-05-11 08:36:09 +0700

    Add files via upload

---
+++ Tugas4/tugasp4/Form_Kelvin/Form_Kelvin/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim K, C, F, R As Decimal
+        K = CDec(txtKelvin.Text)
+        C = ToCelcius(K)
+        F = ToFarenheit(K)
+        R = ToReamur(K)
+
+        txtCelcius.Text = Str(C)
+        txtFarenheit.Text = Str(F)
+        txtReamur.Text = Str(R)
+    End Sub
+
+    Private Function ToCelcius(K As Decimal) As Decimal
+        Dim C As Decimal
+        C = K - 273
+        Return C
+    End Function
+
+    Private Function ToFarenheit(K As Decimal) As Decimal
+        Dim F As Decimal
+        F = 9 / 5 * (K - 273) + 32
+        Return F
+    End Function
+
+    Private Function ToReamur(K As Decimal) As Decimal
+        Dim R As Decimal
+        R = 4 / 5 * (K - 273)
+        Return R
+    End Function
+
+End Class
+
---
+++ Tugas4/tugasp4/Form_Reamur/Form_Reamur/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim R, C, F, K As Decimal
+        R = CDec(txtReamur.Text)
+        C = ToCelcius(R)
+        F = ToFarenheit(R)
+        K = ToKelvin(R)
+
+        txtCelcius.Text = Str(C)
+        txtFarenheit.Text = Str(F)
+        txtKelvin.Text = Str(K)
+    End Sub
+
+    Private Function ToCelcius(R As Decimal) As Decimal
+        Dim C As Decimal
+        C = 5 / 4 * R
+        Return C
+    End Function
+
+    Private Function ToFarenheit(R As Decimal) As Decimal
+        Dim F As Decimal
+        F = (9 / 4) * R + 32
+        Return F
+    End Function
+
+    Private Function ToKelvin(R As Decimal) As Decimal
+        Dim K As Decimal
+        K = 5 / 4 * R + 273
+        Return K
+    End Function
+
+End Class
+
